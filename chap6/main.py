43d6fb8e826f97cb061db202b24f92aa2b6d688f
Ming Gao <gao_ming1977@hotmail.com>
2020-07-03 10:03:33 +0800

    chap6 exercise finished
    1. TensorFlow RNN model ( RNN, GRU, LSTM)
    2. PyTorch RNN model (RNN, GRU, LSTM)
    3. Include model files wich can be used or re-training
    4. Model training process logs

---
+++ chap6/main.py
@@ -0,0 +1,17 @@
+"""
+chapter 6 exercise. Generate Tang peoms with RNN, both implemented by Tensorflow and Pytorch
+reference by : https://blog.csdn.net/Irving_zhang/article/details/76664998
+"""
+
+
+
+
+
+if __name__ == '__main__':
+    print("\n\t\tChapter6. Recurrent Neural Network, RNN exercise ")
+    print("1. RNN of Tensorflow for generating Tang peoms")
+    print("2. RNN of Pytorch for generating Tang peoms")
+    print("other, bye-bye !")
+
+
+    pass
